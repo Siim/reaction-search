a2e3b354bd7ce4995bb7d050d96638e254ecfe58
ScyDev <lukas.saegesser@gmail.com>
2016-02-21 13:01:02 +0100

    - converted from coffee to javascript

--- package.js
+++ package.js
@@ -16,18 +16,18 @@ Package.on_use(function (api, where) {
   ], ["client", "server"]);
 
   api.add_files([
-    //"common/register.coffee"
+    //"common/register.js"
   ],["client","server"]);
 
   api.add_files([
-    //"client/routing.coffee",
+    //"client/routing.js",
     "client/templates/searchBox.html",
     "client/templates/searchBox.less",
     "client/templates/searchBox.js"
   ], ["client"]);
 
   api.add_files([
-    "server/methods.coffee"
+    "server/methods.js"
   ], ["server"]);
 
 });
